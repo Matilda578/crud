146431751107e6b3aa2fe75e6dc7ff5042daf6c5
Matilda578 <tilly.g@hotmail.com>
2020-12-10 13:14:36 +1000

    um

--- Program.cs
+++ Program.cs
@@ -13,110 +13,110 @@ namespace crud
             var context = new MoviesContext();
             var Movieclass = new Movie();
 
-            //  Console.WriteLine("Please enter MovieNo: ");
-            //  var userMovieNo = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("Please enter MovieNo: ");
+              var userMovieNo = Convert.ToInt32(Console.ReadLine());
 
-            //  var numberofActors = Movieclass.NumActors(context,userMovieNo);
-            //  Console.WriteLine(numberofActors);
-
-
-            //  Console.WriteLine("To check a Movie's age please enter the Movie No: ");
-            //  var userMovieAgeCheck = Convert.ToInt16(Console.ReadLine());
-
-            //  var movieAgeResult = Movieclass.GetAge(context, userMovieAgeCheck);
-            //  Console.WriteLine($"The movie is {movieAgeResult} years old.");
+              var numberofActors = Movieclass.NumActors(context,userMovieNo);
+              Console.WriteLine(numberofActors);
 
 
+             Console.WriteLine("To check a Movie's age please enter the Movie No: ");
+              var userMovieAgeCheck = Convert.ToInt16(Console.ReadLine());
 
-            //  List<Movie> movieList = context.Movies.ToList();
-            //  foreach (Movie moobie in movieList)
-            //  {
-            //      Console.WriteLine(moobie.Title, moobie.Runtime);
-            //  }
-
-
-            //  var moviesearch = from movienames in context.Movies
-            //                   where movienames.Title.StartsWith("The")
-            //                    select movienames;
-
-            //  List<Movie> moobiess = moviesearch.ToList();
-            //  foreach (Movie mooovie in moobiess)
-            //  {
-            //      Console.WriteLine(mooovie.Title);
-            //  }
+              var movieAgeResult = Movieclass.GetAge(context, userMovieAgeCheck);
+              Console.WriteLine($"The movie is {movieAgeResult} years old.");
 
 
 
-            //  Console.WriteLine("Please enter Movie Title: ");
-            //  var userupdateMovieselection = Console.ReadLine();
+             List<Movie> movieList = context.Movies.ToList();
+              foreach (Movie moobie in movieList)
+             {
+                  Console.WriteLine(moobie.Title, moobie.Runtime);
+              }
 
-            //  Console.WriteLine("Please enter the new title for the movie: ");
-            //  var Movieuserupdatemovie = Console.ReadLine();
+
+              var moviesearch = from movienames in context.Movies
+                               where movienames.Title.StartsWith("The")
+                                select movienames;
+
+             List<Movie> moobiess = moviesearch.ToList();
+              foreach (Movie mooovie in moobiess)
+              {
+                  Console.WriteLine(mooovie.Title);
+              }
 
 
-            //  var moviequery2 = from Movie in context.Movies
-            //                    where Movie.Title.StartsWith(userupdateMovieselection)
-            //                    select Movie;
 
-            //  var moviequery2result = moviequery2.FirstOrDefault<Movie>();
+             Console.WriteLine("Please enter Movie Title: ");
+              var userupdateMovieselection = Console.ReadLine();
 
-            //  moviequery2result.Title = Movieuserupdatemovie;
-            //  context.SaveChanges();
+              Console.WriteLine("Please enter the new title for the movie: ");
+              var Movieuserupdatemovie = Console.ReadLine();
+
+
+              var moviequery2 = from Movie in context.Movies
+                                where Movie.Title.StartsWith(userupdateMovieselection)
+                                select Movie;
+
+              var moviequery2result = moviequery2.FirstOrDefault<Movie>();
+
+              moviequery2result.Title = Movieuserupdatemovie;
+              context.SaveChanges();
 
             //  ///b
 
-            //  Console.WriteLine("Please enter the actor's fullname: ");
-            //  var updateactorsname = Console.ReadLine();
+            Console.WriteLine("Please enter the actor's fullname: ");
+              var updateactorsname = Console.ReadLine();
 
-            //  Console.WriteLine("Please enter the new  first name for the actor: ");
-            //  var updateactorfirstname = Console.ReadLine();
+              Console.WriteLine("Please enter the new  first name for the actor: ");
+             var updateactorfirstname = Console.ReadLine();
 
-            //  Console.WriteLine("Please enter the new  SURNAME for the actor: ");
-            //  var updateactorsurname = Console.ReadLine();
-
-
-            //  var updateactornamequery = from Actor in context.Actors
-            //                    where Actor.Fullname.StartsWith(updateactorsname)
-            //                    select Actor;
-
-            //var updateactornamequeryresult = updateactornamequery.FirstOrDefault<Actor>();
-
-            //  updateactornamequeryresult.Givenname = updateactorfirstname;
-            //  updateactornamequeryresult.Surname = updateactorsurname;
-            //  context.SaveChanges();
-
-            //  Console.WriteLine("Please add new title: ");
-            //  var addnewMovieTitle = Console.ReadLine();
-
-            //  Console.WriteLine("Please add new Release year: ");
-            //  var addnewMovieRelyear = Convert.ToInt16(Console.ReadLine());
-
-            //  Console.WriteLine("Please add new movienumber: ");
-            //  var addnewMovieNumber = Convert.ToInt32(Console.ReadLine());
-
-            //  Console.WriteLine("Please add new Run Time: ");
-            //  var addnewMovierunTime = Convert.ToInt16(Console.ReadLine());
-
-            //  Movie useraddMovie = new Movie { Title = addnewMovieTitle, Movieno = addnewMovieNumber, Relyear = addnewMovieRelyear, Runtime = addnewMovierunTime };
-
-            //  context.Movies.AddRange(useraddMovie);
-            //  context.SaveChanges();
+              Console.WriteLine("Please enter the new  SURNAME for the actor: ");
+             var updateactorsurname = Console.ReadLine();
 
 
-            //Console.WriteLine("Please add a new Actor given name: ");
-            //var addnewActorgivenname = Console.ReadLine();
+             var updateactornamequery = from Actor in context.Actors
+                                where Actor.Fullname.StartsWith(updateactorsname)
+                               select Actor;
 
-            //Console.WriteLine("Please add new Actor surname : ");
-            //var addnewActorSurname = (Console.ReadLine());
+            var updateactornamequeryresult = updateactornamequery.FirstOrDefault<Actor>();
 
-            //Console.WriteLine("Please add new actor number: ");
-            //var addnewActorNumber = Convert.ToInt32(Console.ReadLine());
+              updateactornamequeryresult.Givenname = updateactorfirstname;
+              updateactornamequeryresult.Surname = updateactorsurname;
+              context.SaveChanges();
+
+              Console.WriteLine("Please add new title: ");
+              var addnewMovieTitle = Console.ReadLine();
+
+              Console.WriteLine("Please add new Release year: ");
+              var addnewMovieRelyear = Convert.ToInt16(Console.ReadLine());
+
+              Console.WriteLine("Please add new movienumber: ");
+             var addnewMovieNumber = Convert.ToInt32(Console.ReadLine());
+
+              Console.WriteLine("Please add new Run Time: ");
+             var addnewMovierunTime = Convert.ToInt16(Console.ReadLine());
+
+              Movie useraddMovie = new Movie { Title = addnewMovieTitle, Movieno = addnewMovieNumber, Relyear = addnewMovieRelyear, Runtime = addnewMovierunTime };
+
+             context.Movies.AddRange(useraddMovie);
+             context.SaveChanges();
 
 
-            //Actor useraddActor = new Actor { Givenname = addnewActorgivenname, Surname = addnewActorSurname, Actorno = addnewActorNumber, Fullname = ($"{addnewActorgivenname}  {addnewActorSurname}") };
+            Console.WriteLine("Please add a new Actor given name: ");
+            var addnewActorgivenname = Console.ReadLine();
 
-            //context.Actors.AddRange(useraddActor);
-            //context.SaveChanges();
+            Console.WriteLine("Please add new Actor surname : ");
+            var addnewActorSurname = (Console.ReadLine());
+
+            Console.WriteLine("Please add new actor number: ");
+            var addnewActorNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            Actor useraddActor = new Actor { Givenname = addnewActorgivenname, Surname = addnewActorSurname, Actorno = addnewActorNumber, Fullname = ($"{addnewActorgivenname}  {addnewActorSurname}") };
+
+            context.Actors.AddRange(useraddActor);
+            context.SaveChanges();
 
             Console.WriteLine("Pleas enter the actor number to be cast: ");
             var castnewActorNo = Convert.ToInt32(Console.ReadLine());
